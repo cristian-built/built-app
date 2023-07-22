1b12b934ab88d2503308c1ca4251f749405df186
cristian-built <139931484+cristian-built@users.noreply.github.com>
2023-07-22 16:45:52 -0700

    Update settings.py

--- built/settings.py
+++ built/settings.py
@@ -27,12 +27,13 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1-*y7*f(-!f23_r11nbra=ms-%0t^25rh7%yiffs1x)7w#b0jz'
+# SECRET_KEY = 'django-insecure-1-*y7*f(-!f23_r11nbra=ms-%0t^25rh7%yiffs1x)7w#b0jz'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '*']
 
 
 # Application definition

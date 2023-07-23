34eb040c300238dd379c4e9208eac556425b5326
cristian-built <139931484+cristian-built@users.noreply.github.com>
2023-07-23 13:56:28 -0700

    Update urls.py

--- built/urls.py
+++ built/urls.py
@@ -27,3 +27,6 @@ urlpatterns = [
     path('logout/', include('django.contrib.auth.urls'), {'next_page': 'account_login'}, name='account_logout'),
     path('__debug__/', include('debug_toolbar.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
+               static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
